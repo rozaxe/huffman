6e2dd797f9b1b6589709b0d1a00bf3f2cc0620ce
Axel Rozo--Brézillac <axel.rozobrezillac@gmail.com>
2016-01-11 19:28:08 +0100

    Add prototype

--- io.h
+++ io.h
@@ -14,3 +14,19 @@ int read_byte();
 
 // Return the next bit read from the file 
 int read_bit();
+
+/*
+ * Writing functions
+ */
+
+// Call this function before writing
+int create_writer();
+
+// Call this function when done writing
+int delete_writer();
+
+// Append the byte to file
+int write_byte(int);
+
+// Append the bit to file
+int write_bit(int);
