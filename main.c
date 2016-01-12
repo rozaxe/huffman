c7511153b74e2a52b9e62ad0115775c019af119c
Axel Rozo--Brézillac <axel.rozobrezillac@gmail.com>
2016-01-12 15:18:31 +0100

    Basic running

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
 
-int main() {
+#include <stdio.h>
+#include "huffman.h"
+
+int main(int argc, char *argv[]) {
+
+    if (argc > 1) {
+        // Argument specified
+
+        // For now, just compress it
+        compress();
+
+    } else {
+        // Tell how to use program
+        printf("You must specified at least one argument !\n");
+    }
+
     return 0;
 }
